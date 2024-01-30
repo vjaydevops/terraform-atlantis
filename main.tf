ee6cea16c2769151ec1cae254c412917c650d9f9
vjaydevops <158208364+vjaydevops@users.noreply.github.com>
2024-01-31 00:37:00 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+profile = "default"
+}
+
+############ SAVING TF STATE FILE #########
+terraform {
+  backend "s3" {
+    bucket = "s3-backend-terraform-project-1"
+    key = "atlantis/terraform.tfstate"
+    region = "ap-south-1"
+    profile = "default"
+  }
+}
+
+################# EC2 INSTANCE CREATION #########
+
+resource "aws_instance" "this" {
+  ami = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-atlantis"
+  }
+}
