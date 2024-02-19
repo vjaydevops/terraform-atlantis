93d04093ae26becc8ed9f6677542fa0ac6203d71
vjaydevops <158208364+vjaydevops@users.noreply.github.com>
2024-02-19 11:31:19 +0530

    Update main.tf

---
+++ stage/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "s3-backend-terraform-project-1"
+    key = "atlantis/terraform.tfstate"
+    region = "ap-south-1"
+    profile = "default"
+  }
+}
+
+resource "aws_instance" "this" {
+  ami = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "$WORKSPACE-ec2"
+  }
+}
