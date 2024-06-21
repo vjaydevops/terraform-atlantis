ddf4bcd6080f967b687218e7a6b8b503f1d39f8f
EC2 Default User <ec2-user@ip-172-31-43-203.ap-south-1.compute.internal>
2024-06-21 01:52:53 +0000

    update AMI id

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "s3-backend-terraform-project-1"
+    bucket = "my-s3-remote-backend-83264873246"
     key = "atlantis/terraform.tfstate"
     region = "ap-south-1"
     profile = "default"
@@ -12,7 +12,7 @@ terraform {
 }
 
 resource "aws_instance" "this" {
-  ami = "ami-03f4878755434977f"
+  ami = "ami-0e1d06225679bc1c5"
   instance_type = "t2.micro"
 
   tags = {
