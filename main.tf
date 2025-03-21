e9c5946c2768b24aca27f8e302f1dc11668d9c16
vjaydevops <158208364+vjaydevops@users.noreply.github.com>
2025-03-21 10:37:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ terraform {
   }
 }
 
+//*
 resource "aws_instance" "this" {
   ami = "ami-0dee22c13ea7a9a67"
   instance_type = "t2.micro"
@@ -19,3 +20,4 @@ resource "aws_instance" "this" {
     Name = "terraform-atlantis-demo"
   }
 }
+*//
