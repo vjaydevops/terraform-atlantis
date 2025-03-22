1829777fc8aaacc0aef93b52e0d0f005a4a383fe
vjaydevops <158208364+vjaydevops@users.noreply.github.com>
2025-03-22 10:23:59 +0530

    Update main.tf
    remove backend

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "my-s3-backend-for-terraform6785759"
-    key = "atlantis/terraform.tfstate"
-    region = "ap-south-1"
-    profile = "default"
-  }
-}
 
 resource "aws_instance" "this" {
   ami = "ami-0dee22c13ea7a9a67"
