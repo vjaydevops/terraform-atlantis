a8c73b8ef20423e970112cd46effa2a6a9b009ac
vjaydevops <158208364+vjaydevops@users.noreply.github.com>
2025-03-22 10:31:21 +0530

    create s3

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-itd-tfc"
+
+  tags = {
+    Name        = "s3-from-tfc"
+    Environment = "Dev"
+  }
+}
 
 resource "aws_instance" "this" {
   ami = "ami-0dee22c13ea7a9a67"
