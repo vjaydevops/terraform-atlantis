ae555a3f5f4b5bc113e9dcde99bbfcffacfb476c
vjaydevops <158208364+vjaydevops@users.noreply.github.com>
2025-03-22 10:23:08 +0530

    Update main.tf
    Create ec2 demo

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "this" {
   ami = "ami-0dee22c13ea7a9a67"
   instance_type = "t2.micro"
   tags = {
-    Name = "terraform-cloud-demo"
+    Name = "terraform-cloud-ec2"
   }
 }
 
