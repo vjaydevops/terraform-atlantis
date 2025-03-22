327db557533dc63ecf4f1ca61a2b8e2d16e9b61c
vjaydevops <158208364+vjaydevops@users.noreply.github.com>
2025-03-22 10:40:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,5 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-itd-tfc"
 
-  tags = {
-    Name        = "s3-from-tfc"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_instance" "this" {
-  ami = "ami-0dee22c13ea7a9a67"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "terraform-cloud-ec2"
-  }
-}
 
