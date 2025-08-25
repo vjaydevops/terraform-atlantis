80ab8adc239a58e046ff2d6ca4f869d001f7da67
vjaydevops <158208364+vjaydevops@users.noreply.github.com>
2025-08-25 11:14:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 resource "aws_instance" "this" {
   ami = "ami-0f918f7e67a3323f0"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   tags = {
-    Name = "terraform-atlantis-demo"
+    Name = "terraform-cloud-ec2-new"
   }
 }
 
